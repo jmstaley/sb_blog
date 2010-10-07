78a02036df6d9472564cf7ffbed7cd002ccc573c
Jon <jmstaley>
2010-10-07 21:57:15 +0100

    Add new features

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from tagging.fields import TagField
 
 class Category(models.Model):
     title = models.CharField(max_length=255, 
@@ -19,8 +21,21 @@ class Category(models.Model):
         return '/categories/%s' % self.slug
 
 class Entry(models.Model):
+    LIVE_STATUS = 1
+    DRAFT_STATUS = 2
+    STATUS_CHOICES = (
+        (LIVE_STATUS, 'Live'),
+        (DRAFT_STATUS, 'Draft')
+    )
+
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=250)
     excerpt = models.TextField(blank=True)
     body = models.TextField()
     pub_date = models.DateTimeField(default=datetime.datetime.now)
     slug = models.SlugField(unique_for_date='pub_date')
+    enable_comments = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
+    categories = models.ManyToManyField(Category)
+    tags = TagField()
